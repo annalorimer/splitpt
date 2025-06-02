f065f1bdb963b18d7831806766a3ee7bfbe3327b
Chameleon Cloud User <annalorimer@uchicago.edu>
2025-06-02 22:38:41 +0000

    adds script to generate trial

--- tests/shadow_pt_tiny/generate_trial.sh
+++ tests/shadow_pt_tiny/generate_trial.sh
@@ -4,4 +4,4 @@ trialdir=$1
 
 mkdir $trialdir
 
-tar -xvf tornet__net-0.01__load-3.2__trial-1__pt-obfs4.tar.xz --directory=$trialdir --strip-components=1
+tar -xvf splitpt-trial.tar --directory=$trialdir --strip-components=1
